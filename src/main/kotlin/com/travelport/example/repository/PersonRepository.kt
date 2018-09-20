93100535914bc8e7a9149602c2109bffa7698f25
gkelly <graham.kelly@travelportdigital.com>
2018-09-20 16:54:27 +0100

    Removed example project

--- src/main/kotlin/com/travelport/example/repository/PersonRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.travelport.example.repository
-
-import com.travelport.example.model.Person
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface PersonRepository : MongoRepository<Person, String>
--- src/test/groovy/com/travelport/example/UnitTest.groovy
+++
@@ -1,7 +0,0 @@
-package com.travelport.example
-
-import spock.lang.Specification
-
-abstract class UnitTest extends Specification {
-
-}
