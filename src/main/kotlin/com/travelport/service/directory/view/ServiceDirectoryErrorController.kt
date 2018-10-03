1ea6e1a6d0272605c9a47e8f153d030311a0dd5d
gkelly <graham.kelly@travelportdigital.com>
2018-10-03 11:57:15 +0100

    Added error controller. Requests for pages generating errors should direct here
    * Added Error controller
    * Added basic CSS formatting
    * Added error pages to be served

---
+++ src/main/kotlin/com/travelport/service/directory/view/ServiceDirectoryErrorController.kt
@@ -0,0 +1,29 @@
+package com.travelport.service.directory.view
+
+import org.springframework.boot.web.servlet.error.ErrorController
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import javax.servlet.RequestDispatcher
+import javax.servlet.http.HttpServletRequest
+
+@Controller
+class ServiceDirectoryErrorController: ErrorController {
+  override fun getErrorPath() = "/error"
+
+  @RequestMapping("/error")
+  fun handleError(request: HttpServletRequest): String {
+    val status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE)
+
+    if (status != null) {
+      val statusCode = Integer.valueOf(status.toString())
+
+      if (statusCode == HttpStatus.NOT_FOUND.value()) {
+        return "error-404.html"
+      } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+        return "error-500.html"
+      }
+    }
+    return "error.html"
+  }
+}
